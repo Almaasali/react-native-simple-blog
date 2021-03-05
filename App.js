4bc25080e2b3efb3bcc20c0b205fe906ce896fee
Almaasalim <almaasali.saeed@gmail.com>
2021-03-05 13:37:08 +0300

    added the style to create/edit screens

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Provider } from "react-redux";
+import { Feather, FontAwesome } from "@expo/vector-icons";
 import IndexScreen from "./src/screens/IndexScreen";
-
 import { store } from "./src/redux/stores";
 import ShowScreen from "./src/screens/ShowScreen";
 import EditBlogScreen from "./src/screens/EditBlogScreen";
 import CreateBlogScreen from "./src/screens/CreateBlogScreen";
+import { StyleSheet, TouchableOpacity } from "react-native";
 
 const Stack = createStackNavigator();
 
@@ -18,15 +19,42 @@ const Navigator = () => {
         <Stack.Screen
           name="Index"
           component={IndexScreen}
-          options={{ title: "Blogs" }}
+          options={({ navigation, route }) => ({
+            title: "Blogs",
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+                <Feather name="plus" style={styles.addIcon} />
+              </TouchableOpacity>
+            ),
+          })}
         />
-        <Stack.Screen name="Show" component={ShowScreen} />
+        <Stack.Screen
+          name="Show"
+          component={ShowScreen}
+          options={({ navigation, route }) => ({
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
+                <FontAwesome name="pencil" style={styles.editIcon} />
+              </TouchableOpacity>
+            ),
+          })}
+        />
         <Stack.Screen name="Edit" component={EditBlogScreen} />
         <Stack.Screen name="Create" component={CreateBlogScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+const styles = StyleSheet.create({
+  addIcon: {
+    fontSize: 30,
+    margin: 5,
+  },
+  editIcon: {
+    fontSize: 30,
+    margin: 5,
+  },
+});
 
 const App = () => {
   return (
--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,14 +1,43 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  Button,
+} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 const BlogPostForm = () => {
   return (
     <View>
-      <Text>Blog Form </Text>
+      <Text style={styles.title}>Enter the title: </Text>
+      <TextInput autoCapitalize={"none"} style={styles.inputStyle} />
+      <Text style={styles.title}>Enter the content: </Text>
+      <TextInput autoCapitalize={"none"} style={styles.inputStyle} />
+      <TouchableOpacity style={styles.saveBtnStyle}>
+        <Button title="Save blog" />
+      </TouchableOpacity>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    margin: 10,
+  },
+  inputStyle: {
+    padding: 10,
+    borderWidth: 1,
+    margin: 10,
+  },
+  saveBtnStyle: {
+    margin: 10,
+    color: "red",
+    fontSize: 18,
+  },
+});
 
 export default BlogPostForm;
